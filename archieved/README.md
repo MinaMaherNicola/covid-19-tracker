d44f393f4a86553c819a25d7584bf10cb645b6fb
Mina Maher <minamahernicola@outlook.com>
2021-05-06 20:03:14 +0200

    Initial commit for refactoring

---
+++ archieved/README.md
@@ -0,0 +1,2 @@
+# covid-19-tracker
+A simple React app that can be used to track covid-19 statistics worldwide.
--- archieved/src/index.js
+++ archieved/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './style/style.css';
+import App from './components/App/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
