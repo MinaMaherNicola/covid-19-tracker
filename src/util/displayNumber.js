5a440e36aa1e8d587da16590accbe7f0f6316927
Mina Maher <minamahernicola@outlook.com>
2021-05-08 02:56:04 +0200

    Better number display

--- src/util/displayNumber.js
+++ src/util/displayNumber.js
@@ -1,13 +1,3 @@
 export const displayNumber = (number = 1) => {
-  number = number.toString().split('');
-  let numberString = '';
-  number.forEach((digit, index) => {
-    if (index % 3 === 0 && index !== 0) {
-      numberString += ',';
-      numberString += digit;
-    } else {
-      numberString += digit;
-    }
-  });
-  return numberString;
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
